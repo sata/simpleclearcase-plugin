866d4e26da0ac2960067e1025830fa47b43b8e3f
Sam Tavakoli <sam@tavakoli.se>
2011-06-10 09:59:36 +0200

    Added check for empty comments in parsing, additional parameter 'event description' is listed now

--- src/main/java/jenkins/plugins/simpleclearcase/ClearTool.java
+++ src/main/java/jenkins/plugins/simpleclearcase/ClearTool.java
@@ -302,9 +302,9 @@ public class ClearTool {
 		if (entryDate == null) {
 			return null; 
 		}
-		
+
 		//the constructor of ChangeLogEntry follows LSHISTORY_FORMATTING parameter order
 		return new SimpleClearCaseChangeLogEntry(entryDate, splitted[1], splitted[2], splitted[3], 
-								splitted[4], splitted[5]);
+								splitted[4], splitted[5], (splitted.length > 6) ? splitted[6] : "");
 	}
 }

b2154caeca92d1a5c853347828bc8e02737b3f27
Sam Tavakoli <sam@tavakoli.se>
2011-06-23 14:15:31 +0200

    removed getMailFromAuthor as its a post SCM-poll function

--- src/main/java/jenkins/plugins/simpleclearcase/util/OsUtil.java
+++ src/main/java/jenkins/plugins/simpleclearcase/util/OsUtil.java
@@ -30,13 +30,4 @@ package jenkins.plugins.simpleclearcase.util;
  */
 public class OsUtil {
 	public static final String WIN_NEWLINE  	   = "\r\n";
-
-	public static String getMailFromAuthor(String author, boolean isUnix) {
-		
-		if (isUnix == false) //currently no support for !unix systems
-			return null;
-		
-		//must retreive mail from shell, how?
-		return null;
-	}
 }
